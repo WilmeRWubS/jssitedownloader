027e9b569a1584cb5f60e7ac4d4320e61926828b
Wilmer Wubs <45149832+WilmeRWubS@users.noreply.github.com>
2023-01-03 15:15:49 +0100

    added footer

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ async function handleRequest(request) {
 
     } else {
         // Create the HTML for the download form
+        const logo = `<svg aria-hidden="true" class="octicon octicon-mark-github" height="32" version="1.1" viewBox="0 0 16 16" width="32"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>`
         const html = `
             <html>
             <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -97,6 +98,17 @@ async function handleRequest(request) {
             </body>
             </div>
             </div>
+            <footer class="bg-dark py-3 mt-5" style="position: fixed; bottom: 0; width: 100%; height: 60px;">
+            <div class="container">
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-auto text-white">
+                        <a href="https://github.com/WilmeRWubS/jssitedownloader">${logo}</a>
+                        <span class="mx-2">|</span>
+                        <p class="d-inline-block m-0">Copyright &copy; 2023 Whocares</p>
+                    </div>
+                </div>
+            </div>
+            </footer>
             </html>
         `;
 
