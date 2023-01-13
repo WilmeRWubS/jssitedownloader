61ff4f1d26f14663bae9a0effddf3c1ae1348c93
Wilmer Wubs <45149832+WilmeRWubS@users.noreply.github.com>
2023-01-13 14:12:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -62,11 +62,9 @@ async function handleRequest(request) {
         const logo = `<svg aria-hidden="true" class="octicon octicon-mark-github" height="32" version="1.1" viewBox="0 0 16 16" width="32"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>`
         const html = `
             <html>
-            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
             <head>
                 <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link rel="icon" href="https://wilmerdrive.nl/0:/serj.ico" type="image/x-icon">
+                <link rel="icon" href="https://wilmerdrive.nl/0:/serjico.ico" type="image/x-icon">
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
                 <title>Wilmers Downloader</title>
             </head>
@@ -91,11 +89,13 @@ async function handleRequest(request) {
             <body>
                 <h1>Wilmers Downloader</h1>
                 <form method="POST">
-                    <label for="link">Geef link:</label><br>
+                    <label for="link">Gib link: (should end with filename.extension)</label><br>
                     <input type="text" id="link" name="link"><br>
                     <br>
                     <button type="submit" class="btn btn-dark">Download</button>
                 </form>
+            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
             </body>
             </div>
             </div>
